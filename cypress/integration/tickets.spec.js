f082693a023d89e9ed23c1c9d9f9d33464c25f9b
thiagoherculan@gmail.com <thiagoherculan@gmail.com>
2021-12-26 18:12:39 -0300

    realizado teste do tipo select no site com sucesso

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,16 +1,21 @@
+
 describe("Tickets", () => {
     beforeEach(() => cy.visit("https://bit.ly/2XSuwCW")); /*Abre a url do site*/
 
-    it.only("preencha todos os campos de entrada de texto", () =>{
+    it("preencha todos os campos de entrada de texto", () =>{
         const primeiroNome = "Thiago";
         const segundoNome = "Herculano";
 
-        cy.get("#first-name").type(primeiroNome);
+        cy.get("#first-name").type(primeiroNome);        /*interagindo com elemento do tipo texto*/
         cy.get("#last-name").type(segundoNome);
         cy.get("#email").type("thiagoherculan@gmail.com");
         cy.get("#requests").type("Vegetarian");
         cy.get("#signature").type(`${primeiroNome} ${segundoNome}`);
     })
+
+    it.only("seleciona 2 tickets", () =>{
+        cy.get("#ticket-quantity").select("2");   /* Interagindo com elemento do tipo select*/
+    });
 
     it("has 'TICKETBOX' header's heading", () =>{});
 });
