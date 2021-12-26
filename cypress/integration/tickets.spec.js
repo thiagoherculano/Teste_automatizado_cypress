df4da29debfcefb6ac9ac1414ad10393ddc63259
thiagoherculan@gmail.com <thiagoherculan@gmail.com>
2021-12-26 13:43:19 -0300

    feito testes de digitação de campos de texto

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,5 +1,16 @@
 describe("Tickets", () => {
-    beforeEach(() => cy.visit("https://bit.ly/2XSuwCW"));
+    beforeEach(() => cy.visit("https://bit.ly/2XSuwCW")); /*Abre a url do site*/
+
+    it.only("preencha todos os campos de entrada de texto", () =>{
+        const primeiroNome = "Thiago";
+        const segundoNome = "Herculano";
+
+        cy.get("#first-name").type(primeiroNome);
+        cy.get("#last-name").type(segundoNome);
+        cy.get("#email").type("thiagoherculan@gmail.com");
+        cy.get("#requests").type("Vegetarian");
+        cy.get("#signature").type(`${primeiroNome} ${segundoNome}`);
+    })
 
     it("has 'TICKETBOX' header's heading", () =>{});
 });
