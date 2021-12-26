6afdcbbe3687dd95fffa79ab873b7dc4429451c3
thiagoherculan@gmail.com <thiagoherculan@gmail.com>
2021-12-26 13:14:01 -0300

    inserindo a URL da pagina

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,5 +1,5 @@
-const { describe } = require("mocha");
-
 describe("Tickets", () => {
-    
+    beforeEach(() => cy.visit("https://bit.ly/2XSuwCW"));
+
+    it("has 'TICKETBOX' header's heading", () =>{});
 });
